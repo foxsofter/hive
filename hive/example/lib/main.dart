9d40cf33c93c50c4d4d3a194a302a6b78ef7f429
foxsofter <foxsofter@gmail.com>
2023-08-14 10:44:53 +0800

    chore: test open box

--- hive/example/lib/main.dart
+++ hive/example/lib/main.dart
@@ -42,23 +42,45 @@ class Person2 {
   }
 }
 
-
-
 void main() async {
   var path = Directory.current.path;
   Hive
     ..init(path)
+    ..registerAdapter(PersonAdapter())
     ..registerAdapter(Person2Adapter());
 
   var box = await Hive.openBox('testBox');
 
-  var person = Person2(
+  var person = Person(
+    name: 'Peter',
+    age: 21,
+    friends: ['Linda', 'Marc', 'Anne'],
+  );
+  await box.put('peter', person);
+  var person2 = Person2(
     name: 'Dave',
     age: 22,
     friends: ['Linda', 'Marc', 'Anne'],
   );
 
-  await box.put('dave', person);
+  await box.put('dave', person2);
 
+  final p1s = <Person>[];
+  for (var i =0; i < 100000; i++) {
+    p1s.add(Person(name: 'fewfwefewfwef$i', age: 1100+i, friends:  ['Linda', 'Marc', 'Anne']));
+  }
+  box.addAll(p1s);
+
+  print(box.get('peter')); // Peter: 21
   print(box.get('dave')); // Dave: 22
+  print(box.get(1)); // Dave: 22
+
+  await box.close();
+  final sw = Stopwatch();
+  sw.start();
+  var lb = await Hive.openBox('testBox');
+  sw.stop();
+  print("time cost: ${sw.elapsedMilliseconds}");
+  print(lb.get('peter')); // Peter: 21
+  print(lb.get('dave')); // Dave: 22
 }
